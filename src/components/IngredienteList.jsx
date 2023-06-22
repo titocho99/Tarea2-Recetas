7ea324141cbb6ed6076c305195f427c581041177
ArturoSV <79242433+titocho99@users.noreply.github.com>
2023-06-22 10:47:56 -0600

    0.1

--- src/components/IngredienteList.jsx
+++ src/components/IngredienteList.jsx
@@ -2,16 +2,15 @@ const IngredienteList = ({ name, ingredientes, tiempo, image }) => {
   return (
     <>
       <div className="mt-6 mr-4">
-        <img className="h-40 w-60 " src={image} alt="" />
-
-        <h1>
+      <h1>
           <strong>Receta:</strong>
           {name}
         </h1>
+        <img className="h-40 w-60 " src={image} alt="" />
         <h2>
           <strong>Ingredientes:</strong>
         </h2>
-        <ul className="list-disc marker:text-sky-400  pl-2 text-slate-500">
+        <ul className="list-disc list-inside marker:text-sky-400  pl-5 text-slate-500">
           {ingredientes.map((ingrediente, i) => (
             <li key={i} className="py-2 flex">
               {ingrediente.item}
